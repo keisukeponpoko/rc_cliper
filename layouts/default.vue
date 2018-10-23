cde70ea64fe39339892c3a75536988639477c8c9
keisukeponpoko <k.suke.jp1990@gmail.com>
2018-10-23 16:55:38 +0900

    bug_fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
-  <div style="width: 400px; margin: 0 auto;">
-    <nuxt/>
+  <div>
+    <header>
+      <div class="header">
+        <a href="https://roomclip.jp">
+          <img src="https://roomclip.jp/assets/img/1703/logo/logo_roomclip.png">
+        </a>
+      </div>
+    </header>
+    <nuxt class="contents" />
+    <footer>
+      (c) 2018 RoomClip Inc.
+    </footer>
   </div>
 </template>
 
@@ -22,5 +32,29 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
+}
+
+header {
+  box-shadow: 0 0.1rem 0 0 rgba(0, 0, 0, 0.1);
+}
+
+.header {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.header img {
+  margin: 1rem 0 .5rem 1.6rem;
+}
+
+.contents {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+footer {
+  font-size: 1.2rem;
+  color: #443311;
+  margin: 2.4rem 0;
+  text-align: center;
 }
 </style>
--- pages/result/_id.vue
+++ pages/result/_id.vue
@@ -1,7 +1,27 @@
 <template lang="pug">
   div
+    #fb-root &nbsp;
+    script
+      | (function(d, s, id) {
+      | var js, fjs = d.getElementsByTagName(s)[0];
+      | if (d.getElementById(id)) return;
+      | js = d.createElement(s); js.id = id;
+      | js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+      | fjs.parentNode.insertBefore(js, fjs);
+      | }(document, 'script', 'facebook-jssdk'));
+
     .contents
       p.title {{ text }}
+
+      .share
+        .share_child
+          a.twitter-share-button(href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-show-count="false") Tweet
+            script(async src="https://platform.twitter.com/widgets.js" charset="utf-8")
+
+        .share_child
+          .fb-share-button(data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small" data-mobile-iframe="true")
+            a.fb-xfbml-parse-ignore(target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse")
+
       ul.images
         li.image(v-for='image in images')
           img.img(:src="'https://cdn.roomclip.jp/v1/640/roomclip-bucket/img_640/' + image + '.jpg'")
@@ -29,13 +49,13 @@ export default {
     ];
 
     const TAG_IDS = [
-      2100, 2296, 2517, 100672, 70903, 25144, 95766, 14339, 209607
+      '3x2100', '3x2296', '3x2517', '3x100672', '3x70903', '3x25144', '3x95766', '3x14339', '3x209607'
     ];
 
     if (!params.id) {
       return {
         text: 'あなたに合うインテリアを探してみよう！',
-        id: 3
+        id: '3'
       };
     }
 
@@ -61,7 +81,6 @@ export default {
 
 <style lang="scss" scoped>
 .contents {
-  margin: 3rem auto;
   text-align: center;
 }
 
@@ -91,6 +110,16 @@ export default {
 
   &:hover {
     opacity: 0.6;
+  }
+}
+
+.share {
+  margin: .5rem;
+  display: flex;
+  justify-content: center;
+
+  .share_child {
+    margin: .2rem;
   }
 }
 
